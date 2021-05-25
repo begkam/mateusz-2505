0da5fefd2d18dbb7b9bfc6a716677e7511797f7e
begkam <84787820+begkam@users.noreply.github.com>
2021-05-25 16:35:28 +0200

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -63,6 +63,30 @@ maqueen.IR_callbackUser(function ({ myparam: message }) {
         music.playMelody("C5 A G E G F B C5 ", 120)
     }
 })
+let BLUE = 0
+let GREEN = 0
+let RED = 0
+let item = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    RED = 0
+    GREEN = 0
+    BLUE = 255
+    for (let index = 0; index < 255; index++) {
+        RED += 1
+        BLUE += -1
+        item.showColor(neopixel.rgb(RED, GREEN, BLUE))
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 255; index++) {
+        GREEN += 1
+        RED += -1
+        item.showColor(neopixel.rgb(RED, GREEN, BLUE))
+        basic.pause(1000)
+    }
+    for (let index = 0; index < 255; index++) {
+        BLUE += 1
+        GREEN += -1
+        item.showColor(neopixel.rgb(RED, GREEN, BLUE))
+        basic.pause(1000)
+    }
 })
