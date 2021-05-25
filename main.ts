4c417cfec2798c7bf994cf7253690f895fc812f4
begkam <84787820+begkam@users.noreply.github.com>
2021-05-25 16:08:13 +0200

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,68 @@
+maqueen.IR_callbackUser(function ({ myparam: message }) {
+    if (message == 64) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (message == 65) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 150)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 150)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (message == 7) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (message == 6) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (message == 64) {
+        maqueen.motorStopAll()
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (message == 9) {
+        music.playMelody("C5 A G E G F B C5 ", 120)
+    }
+})
+basic.forever(function () {
+	
+})
